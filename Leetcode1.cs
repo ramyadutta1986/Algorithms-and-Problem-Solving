7e409a3a96242bf12fabeee157c0e9c97f191905
Ramya Jyothi Dutta <ramyakambala@gmail.com>
2021-03-25 11:02:24 -0700

    Update Leetcode1.cs

--- Leetcode1.cs
+++ Leetcode1.cs
@@ -148,6 +148,22 @@ namespace Test
             }
             return len =j;
         }
+        
+        private static bool IsPalindromenumber(int x)
+        {
+            if((x!=0 && x%10==0) || x<0)
+            {
+            return false;
+            }
+        int revnum=0;
+        while(x>revnum)
+        {
+            revnum=revnum*10+(x%10);
+            x=x/10;
+            
+        }
+        return x==revnum || x==revnum/10;
+        }
 
         private static int StrStr(string haystack, string needle)
         {
